8e51656d6bdebd9e71f1a72155ba9c105fe7a255
Duong-Dao <77279720+Duong-Dao@users.noreply.github.com>
2022-04-20 17:37:46 +0700

    Update index.js

--- src/screens/Setting/index.js
+++ src/screens/Setting/index.js
@@ -1,12 +1,48 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import {
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native
 
-const Setting = () => {
+const titleCategorySetting = [
+  'Account',
+  'Notifiycations',
+  'Theme',
+  'Language',
+  'Feedback',
+]
+
+const renderTitleSetting = ({ item }) => {
   return (
-    <View>
-      <Text>Setting</Text>
-    </View>
+    <TouchableOpacity
+      style={styles.btnItemSetting}
+      onPress={() => console.log('name', item)}>
+      <Text>{item}</Text>
+    </TouchableOpacity>
   )
 }
 
+const Setting = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text>Setting</Text>
+      </View>
+      <View style={styles.main}>
+        <FlatList
+          data={titleCategorySetting}
+          keyExtractor={(i) => i.index}
+          renderItem={renderTitleSetting}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
+      <View style={styles.bottom}>
+        <Text>Bottom</Text>
+      </View>
+    </View>
+  )
+}
 export default Setting
